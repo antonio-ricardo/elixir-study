9cc45cc2b77a638884e463d3cc2a2d7a1d139f42
Antonio Ricardo de Oliveira Filho <antonio.gugou@gmail.com>
2023-02-25 18:07:23 -0300

    fix: puting the right version of elixir in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Inmana.MixProject do
     [
       app: :inmana,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.14.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
